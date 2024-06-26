33cfe3c1c5a4a266d5381ead74c0c6ab568db617
Somdutt <admobid0011@gmail.com>
2024-06-26 21:32:24 +0530

    Updates and bug fixes

--- src/Components/Pages/RecievedMail.js
+++ src/Components/Pages/RecievedMail.js
@@ -1,60 +1,86 @@
-import {  Modal, Table, Button } from "react-bootstrap";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useCallback, useMemo } from "react";
+import { Modal, Table, Button } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
 import { TableBody } from "@mui/material";
-import { useDispatch } from "react-redux";
-import {  deleteReceivedMailHandler, receivedMessageSeen } from "../../Store/DataActions";
+import { deleteReceivedMailHandler, receivedMessageSeen } from "../../Store/DataActions";
+
+// Extract Modal Component and Memoize it
+const ModalData = React.memo(({ showModal, mailData, closeHandler }) => {
+  return (
+    <Modal show={showModal} onHide={closeHandler}>
+      <Modal.Header>{<h2>{mailData.email}</h2>}</Modal.Header>
+      <Modal.Body>{mailData.message}</Modal.Body>
+      <Modal.Footer className="justify-content-center">
+        <Button variant="secondary" onClick={closeHandler}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+});
+
 const RecievedMail = () => {
-  const dispatch = useDispatch()
-  let inboxMails = useSelector((state) => state.data.inboxData);
+  const dispatch = useDispatch();
+  const inboxMails = useSelector((state) => state.data.inboxData);
   const [showModal, setShowModal] = useState(false);
-  const [mailData, setMailData] = useState({})  
-  const closeHandler = () => {setShowModal(false)}
+  const [mailData, setMailData] = useState({});
 
-  const tableHandler = async (mail) => {
-    setMailData(mail)
-    setShowModal(true)
-    if (mail.status === false) {
-       dispatch(receivedMessageSeen(mail))
-    } else {
-      setMailData(mail)
-      setShowModal(true)
-    }
-  };
+  const closeHandler = useCallback(() => {
+    setShowModal(false);
+  }, []);
 
-  const deleteHandler = async(mail) => {
-     dispatch(deleteReceivedMailHandler(mail.key))
-  };
+  const tableHandler = useCallback(
+    (mail) => {
+      setMailData(mail);
+      setShowModal(true);
+      if (mail.status === false) {
+        dispatch(receivedMessageSeen(mail));
+      }
+    },
+    [dispatch]
+  );
 
+  const deleteHandler = useCallback(
+    (mail, event) => {
+      event.stopPropagation(); // Prevent triggering the row click event
+      dispatch(deleteReceivedMailHandler(mail.key));
+    },
+    [dispatch]
+  );
 
-  const ModalData = () => {
-    return (
-      <Modal show={showModal} onHide = {closeHandler}>
-        <Modal.Header>{<h2>{mailData.email}</h2>}</Modal.Header>
-        <Modal.Body>{mailData.message}</Modal.Body>
-        <Modal.Footer className=" justify-content-center">
-          <Button variant='secondary' onClick={closeHandler}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    )
-  }
+  const memoizedInboxMails = useMemo(
+    () =>
+      inboxMails.map((mail) => (
+        <tr onClick={() => tableHandler(mail)} key={mail.key} style={{ width: "80.3vw", display: "flex" }}>
+          <td>{mail.status ? "⚪" : "🔵"}</td>
+          <td style={{ width: "16rem" }}>{mail.email}</td>
+          <td style={{ width: "12rem" }}>{mail.subject}</td>
+          <td style={{ width: "40rem" }}>{mail.message}</td>
+          <td>
+            <Button
+              variant="warning"
+              style={{ height: "2.5rem" }}
+              className="mt-.2"
+              size="sm"
+              id="button"
+              onClick={(e) => deleteHandler(mail, e)}
+            >
+              Delete
+            </Button>
+          </td>
+        </tr>
+      )),
+    [inboxMails, tableHandler, deleteHandler]
+  );
 
   return (
     <>
-    <Table  hover>
-      {inboxMails.map((mail) => (
-        <TableBody key={Math.random()} style={{ width: "80.3vw", display:'flex'}}>
-            <tr onClick={tableHandler.bind(this, mail)} key={Math.random()}>
-            <td >{mail.status ? "⚪" : "🔵"}</td>
-            <td style={{width:'16rem'}}>{mail.email}</td>
-            <td style={{width:'12rem'}}>{mail.subject}</td> 
-            <td style={{width:'40rem'}}>{mail.message}</td>
-            </tr> 
-            <Button variant="warning" style={{height:'2.5rem'}} className='mt-.2' size="sm" id="button" onClick={deleteHandler.bind(this, mail)} >Delete</Button>
+      <Table hover>
+        <TableBody>
+          {memoizedInboxMails}
         </TableBody>
-      ))}
-    </Table>
-    {showModal ? <ModalData />: null}
+      </Table>
+      {showModal && <ModalData showModal={showModal} mailData={mailData} closeHandler={closeHandler} />}
     </>
   );
 };
--- src/Components/Pages/SentBox.js
+++ src/Components/Pages/SentBox.js
@@ -1,61 +1,83 @@
-import {  Modal, Table, Button } from "react-bootstrap";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useCallback, useMemo } from "react";
+import { Modal, Table, Button } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
 import { TableBody } from "@mui/material";
-import { useDispatch } from "react-redux";
 import { deleteSentMailHandler, sentMessageSeen } from "../../Store/DataActions";
+
+const ModalData = React.memo(({ showModal, mailData, closeHandler }) => {
+  return (
+    <Modal show={showModal} onHide={closeHandler}>
+      <Modal.Header>{<h2>{mailData.email}</h2>}</Modal.Header>
+      <Modal.Body>{mailData.message}</Modal.Body>
+      <Modal.Footer className="justify-content-center">
+        <Button variant="secondary" onClick={closeHandler}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+});
+
 const SentBox = () => {
-  const dispatch = useDispatch()
-  let sentMails = useSelector((state) => state.data.sentData);
+  const dispatch = useDispatch();
+  const sentMails = useSelector((state) => state.data.sentData);
   const [showModal, setShowModal] = useState(false);
-  const [mailData, setMailData] = useState({})  
-  const closeHandler = () => {setShowModal(false)}
+  const [mailData, setMailData] = useState({});
 
-  const tableHandler = async (mail) => {
-    setMailData(mail)
-    setShowModal(true)
-    if (mail.status === false) {
-      dispatch(sentMessageSeen(mail))
-    } else {
-      setMailData(mail)
-      setShowModal(true)
-    }
-  };
+  const closeHandler = useCallback(() => {
+    setShowModal(false);
+  }, []);
 
-  const deleteHandler = (mail) => {
-     dispatch(deleteSentMailHandler(mail.key))
-  };
+  const tableHandler = useCallback(
+    (mail) => {
+      setMailData(mail);
+      setShowModal(true);
+      if (mail.status === false) {
+        dispatch(sentMessageSeen(mail));
+      }
+    },
+    [dispatch]
+  );
 
+  const deleteHandler = useCallback(
+    (mail, event) => {
+      event.stopPropagation(); 
+      dispatch(deleteSentMailHandler(mail.key));
+    },
+    [dispatch]
+  );
 
-  const ModalData = () => {
-    console.log(mailData)
-    return (
-      <Modal show={showModal} onHide = {closeHandler}>
-        <Modal.Header>{<h2>{mailData.email}</h2>}</Modal.Header>
-        <Modal.Body>{mailData.message}</Modal.Body>
-        <Modal.Footer className=" justify-content-center">
-          <Button variant='secondary' onClick={closeHandler}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    )
-  }
+  const memoizedSentMails = useMemo(
+    () =>
+      sentMails.map((mail) => (
+        <tr onClick={() => tableHandler(mail)} key={mail.key} style={{ width: "80.3vw", display: "flex" }}>
+          <td>{mail.status ? "⚪" : "🔵"}</td>
+          <td style={{ width: "16rem" }}>{mail.email}</td>
+          <td style={{ width: "12rem" }}>{mail.subject}</td>
+          <td style={{ width: "40rem" }}>{mail.message}</td>
+          <td>
+            <Button
+              variant="warning"
+              style={{ height: "2.5rem" }}
+              className="mt-.2"
+              size="sm"
+              id="button"
+              onClick={(e) => deleteHandler(mail, e)}
+            >
+              Delete
+            </Button>
+          </td>
+        </tr>
+      )),
+    [sentMails, tableHandler, deleteHandler]
+  );
 
   return (
     <>
-    <Table  hover>
-      {sentMails.map((mail) => (
-        <TableBody key={Math.random()} style={{ width: "80.3vw", display:'flex'}}>
-            <tr onClick={tableHandler.bind(this, mail)} key={Math.random()}>
-            <td >{mail.status ? "⚪" : "🔵"}</td>
-            <td style={{width:'16rem'}}>{mail.email}</td>
-            <td style={{width:'12rem'}}>{mail.subject}</td> 
-            <td style={{width:'40rem'}}>{mail.message}</td>
-            </tr> 
-            <Button variant="warning" style={{height:'2.5rem'}} className='mt-.2' size="sm" id="button" onClick={deleteHandler.bind(this, mail)} >Delete</Button>
-        </TableBody>
-      ))}
-    </Table>
-    {showModal ? <ModalData />: null}
+      <Table hover>
+        <TableBody>{memoizedSentMails}</TableBody>
+      </Table>
+      {showModal && <ModalData showModal={showModal} mailData={mailData} closeHandler={closeHandler} />}
     </>
   );
 };
